f946cc10742f1da55602813fde44b3e4e5a58d87
Indrasenareddy4966 <163498141+Indrasenareddy4966@users.noreply.github.com>
2025-02-28 13:48:25 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CatGallery from "./CatGallery";
+
+function App() {
+  return (
+    <div className="App">
+      <CatGallery />
+    </div>
+  );
+}
+
+export default App;
